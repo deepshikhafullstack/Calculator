db12adf64c472bb1f88f5637eb0f4ddbea6bc01f
Deepshikha Banerjee <deepshikhabanerjee4@gmail.com>
2020-02-08 19:27:18 +0530

    calculator

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+  
+    
+    <Calculator/>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
